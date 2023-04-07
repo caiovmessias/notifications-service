868c9016b2bfd19c50fc47a80dbf99c168e4d70e
Caio Messias <caiov.messias@icloud.com>
2023-04-06 23:15:30 -0300

    Finaliza projeto

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { NotificationsController } from './controllers/notifications.controller';
+import { SendNotification } from 'src/app/use-cases/send-notification';
+import { DatabaseModule } from '../database/database.module';
+import { CancelNotification } from '@app/use-cases/cancel-notification';
+import { CountRecipientNotification } from '@app/use-cases/count-recipient-notifications';
+import { ReadNotification } from '@app/use-cases/read-notification';
+import { UnreadNotification } from '@app/use-cases/unread-notification';
+import { GetRecipientNotification } from '@app/use-cases/get-recipient-notifications';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [NotificationsController],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotification,
+    ReadNotification,
+    UnreadNotification,
+    GetRecipientNotification,
+  ],
+})
+export class HttpModule {}
